0046b1480efed183edf015c84880a9a6a14f05a8
lijun <lj@simpletour.com>
2016-05-17 18:14:10 +0800

    增加spring data

---
+++ mybatis/src/main/java/com/leeo/mybatis/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.leeo.mybatis.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by lijun on 2016/5/17.
+ */
+@Controller
+public class HomeController {
+}
