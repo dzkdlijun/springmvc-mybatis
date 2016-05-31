56a1a0aa7e79b4d7ed79112c8de436fd0608fe0a
lijun <lj@simpletour.com>
2016-05-31 18:29:37 +0800

    基本crud成功

--- spring-data/src/main/java/com/leeo/springdata/dao/DepartmentRepository.java
+++ spring-data/src/main/java/com/leeo/springdata/dao/DepartmentRepository.java
@@ -2,11 +2,9 @@ package com.leeo.springdata.dao;
 
 import com.leeo.springdata.domain.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by lijun on 2016/5/30.
  */
-@Component
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
 }
