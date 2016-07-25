f84d342460ff779b2c410f676c148bf1a2f30e05
lijun <lj@simpletour.com>
2016-07-25 18:35:55 +0800

    bootstrap

---
+++ spring-data/src/main/webapp/static/script/out.js
@@ -0,0 +1,29 @@
+/**
+ * Created by lijun on 2016/7/22.
+ */
+alert("hello world!");
+x = 5+5 ;
+document.write(x);
+
+y="6"+"6"; // 字符串 + 字符串
+document.write(y);
+m=7+"7";  // 数字 + 字符串
+document.write(m);
+n="8"+8;  // 字符串 + 数字
+document.write(n);
+
+document.write("<br>")
+
+function my_func(){
+    document.write(1);
+}
+
+my_func()
+
+document.write("<br>");
+
+function my_func(){
+    document.write(2);
+}
+
+my_func()
