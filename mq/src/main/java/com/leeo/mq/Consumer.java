4e4cfb5f85bd027c86a8fca9692e555fb9c66c2f
lijun <lj@simpletour.com>
2017-07-21 18:37:56 +0800

    增加nio、aio demo

---
+++ mq/src/main/java/com/leeo/mq/Consumer.java
@@ -0,0 +1,8 @@
+package com.leeo.mq;
+
+/**
+ * @author lijun
+ * @since 2017/7/21 18:26
+ */
+public class Consumer {
+}
---
+++ mq/src/main/java/com/leeo/mq/Producer.java
@@ -0,0 +1,40 @@
+package com.leeo.mq;
+
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+
+/**
+ * @author lijun
+ * @since 2017/7/21 18:26
+ */
+public class Producer {
+    public static void main(String[] args) {
+        ConnectionFactory cf = new CachingConnectionFactory();
+        RabbitAdmin admin = new RabbitAdmin(cf);
+        Queue queue = new Queue("myQueue");
+        admin.declareQueue(queue);
+        TopicExchange exchange = new TopicExchange("myExchange");
+        admin.declareExchange(exchange);
+        admin.declareBinding(
+                BindingBuilder.bind(queue).to(exchange).with("foo.*"));
+
+        // set up the listener and container
+        SimpleMessageListenerContainer container =
+                new SimpleMessageListenerContainer(cf);
+        Object listener = new Object() {
+            public void handleMessage(String foo) {
+                System.out.println(foo);
+            }
+        };
+        MessageListenerAdapter adapter = new MessageListenerAdapter(listener);
+        container.setMessageListener(adapter);
+        container.setQueueNames("myQueue");
+        container.start();
+    }
+}
