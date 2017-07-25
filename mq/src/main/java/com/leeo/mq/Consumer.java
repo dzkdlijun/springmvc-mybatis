e8a529a3162217fc0d708682694ef5b5cdb08410
lijun <lj@simpletour.com>
2017-07-25 18:05:12 +0800

    增加nio、aio demo

--- mq/src/main/java/com/leeo/mq/Consumer.java
+++ mq/src/main/java/com/leeo/mq/Consumer.java
@@ -1,8 +1,30 @@
 package com.leeo.mq;
 
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
 /**
  * @author lijun
  * @since 2017/7/21 18:26
  */
-public class Consumer {
+@Component
+public class Consumer implements InitializingBean {
+
+    @Value("${amqp.username}")
+    String username;
+    @Value("${amqp.password}")
+    String password;
+
+
+    public void handle(String msg){
+        System.out.println("pring mqMessage :"+msg);
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("username:"+username);
+        System.out.println("password:"+password);
+    }
 }
--- mq/src/main/java/com/leeo/mq/Producer.java
+++ mq/src/main/java/com/leeo/mq/Producer.java
@@ -6,8 +6,11 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @author lijun
@@ -15,26 +18,32 @@ import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
  */
 public class Producer {
     public static void main(String[] args) {
-        ConnectionFactory cf = new CachingConnectionFactory();
-        RabbitAdmin admin = new RabbitAdmin(cf);
-        Queue queue = new Queue("myQueue");
-        admin.declareQueue(queue);
-        TopicExchange exchange = new TopicExchange("myExchange");
-        admin.declareExchange(exchange);
-        admin.declareBinding(
-                BindingBuilder.bind(queue).to(exchange).with("foo.*"));
+//        ConnectionFactory cf = new CachingConnectionFactory();
+//        RabbitAdmin admin = new RabbitAdmin(cf);
+//        Queue queue = new Queue("myQueue");
+//        admin.declareQueue(queue);
+//        TopicExchange exchange = new TopicExchange("myExchange");
+//        admin.declareExchange(exchange);
+//        admin.declareBinding(
+//                BindingBuilder.bind(queue).to(exchange).with("foo.*"));
+//
+//        // set up the listener and container
+//        SimpleMessageListenerContainer container =
+//                new SimpleMessageListenerContainer(cf);
+//        Object listener = new Object() {
+//            public void handleMessage(String foo) {
+//                System.out.println(foo);
+//            }
+//        };
+//        MessageListenerAdapter adapter = new MessageListenerAdapter(listener);
+//        container.setMessageListener(adapter);
+//        container.setQueueNames("myQueue");
+//        container.start();
 
-        // set up the listener and container
-        SimpleMessageListenerContainer container =
-                new SimpleMessageListenerContainer(cf);
-        Object listener = new Object() {
-            public void handleMessage(String foo) {
-                System.out.println(foo);
-            }
-        };
-        MessageListenerAdapter adapter = new MessageListenerAdapter(listener);
-        container.setMessageListener(adapter);
-        container.setQueueNames("myQueue");
-        container.start();
+        AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        RabbitTemplate template = ctx.getBean(RabbitTemplate.class);
+        for(int i=0;i<10;i++){
+            template.convertAndSend(String.format("第%s条消息！！",i));
+        }
     }
 }
